581939b312246b337e9c9ca60361ba719fcf4ac4
Amit Chaudhary <studenton.com@gmail.com>
2017-04-11 10:49:43 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var year = today.getFullYear();
 var month = today.getMonth() + 1; // Since its zero indexed
-var month = month < 10 ? '0' + month:  month;
+month = month < 10 ? '0' + month:  month;
 var date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
 
 var url = `http://epaper.ekantipur.com/epaper/the-kathmandu-post/${year}-${month}-${date}/${year}-${month}-${date}.pdf`;
