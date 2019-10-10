9d9dc3d247ef52f73a44eb2799726352d53d245a
Amit Chaudhary <meamitkc@gmail.com>
2019-10-10 16:26:09 +0545

    Update URL due to API changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var today = new Date();
 var year = today.getFullYear();
 var month = today.getMonth() + 1; // Since its zero indexed
+// Apply zero padding to month and day
 month = month < 10 ? '0' + month : month;
 var date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
-
-var url = 'http://epaper.ekantipur.com/epaper/the-kathmandu-post/' + year + '-' + month + '-' + date + '/' + year + '-' + month + '-' + date + '.pdf';
+var url = 'https://epaper.ekantipur.com/kathmandupost/download/' + year + '-' + month + '-' + date;
 
 window.location.href = url;
