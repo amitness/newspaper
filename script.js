89c5cbab3543d02c632645c6391bce3647ce0ea0
UtsavRatna <utsav.ratna@gmail.com>
2019-10-25 15:25:34 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function zeroPad(number) {
+  // Add zero padding for number less than 10
+  return number < 10 ? '0' + number : number;
+}
+
 var today = new Date();
 var year = today.getFullYear();
 var month = today.getMonth() + 1; // Since its zero indexed
-// Apply zero padding to month and day
-month = month < 10 ? '0' + month : month;
-var date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
+month = zeroPad(month);
+var date = zeroPad(today.getDate());
 var url = 'https://epaper.ekantipur.com/kathmandupost/download/' + year + '-' + month + '-' + date;
 
+// Redirect to the newspaper PDF
 window.location.href = url;
