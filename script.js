873b25747d35112d439a4f2a5ac5b529b510366f
utsav ratna <utsav.ratna@gmail.com>
2020-10-01 00:01:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var year = today.getFullYear();
 var month = today.getMonth() + 1; // Since its zero indexed
 // Apply zero padding to month and day
 month = month < 10 ? '0' + month : month;
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+console.log(capitalizeFirstLetter('captittalizethisss'))
+
 var date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
 var url = 'https://epaper.ekantipur.com/kathmandupost/download/' + year + '-' + month + '-' + date;
 
